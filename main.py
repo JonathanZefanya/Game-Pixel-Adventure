300d51a4b4f4ebe7d273240d5c6b87e6d0231255
Xead <jonathan.natannael@student.iti.ac.id>
2024-05-19 12:54:38 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame, sys
+from settings import *
+from level import Level
+# from debug import debug
+
+
+class Game:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption('The Legend Of Hero')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+        # sound
+        main_sound = pygame.mixer.Sound('./audio/main.ogg')
+        main_sound.set_volume(0.5)
+        main_sound.play(loops = -1) # play continuosly
+        
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_m:
+                        self.level.toggle_menu()
+
+            self.screen.fill(WATER_COLOR) # fills the area outside of the island with water color
+            self.level.run()
+            # debug('hello :)')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
